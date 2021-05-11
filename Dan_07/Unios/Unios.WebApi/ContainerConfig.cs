b151fada0355db9c5eaab300ad9c28f04b828b30
Petar Poljarevic <ppoljare@mathos.hr>
2021-05-11 15:09:35 +0200

    Dan 7 - Created DIModules

--- Dan_07/Unios/Unios.WebApi/ContainerConfig.cs
+++ Dan_07/Unios/Unios.WebApi/ContainerConfig.cs
@@ -1,12 +1,6 @@
 ﻿using Autofac;
 using Autofac.Integration.WebApi;
 using System.Reflection;
-using Unios.Model;
-using Unios.Model.Common;
-using Unios.Repository;
-using Unios.Repository.Common;
-using Unios.Service;
-using Unios.Service.Common;
 
 namespace Unios.WebApi
 {
@@ -18,18 +12,8 @@ namespace Unios.WebApi
 
             builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
 
-            builder.RegisterType<Student>().As<IStudent>();
-            builder.RegisterType<StudentEntity>().As<IStudentEntity>();
-            builder.RegisterType<StudentInput>().As<IStudentInput>();
-            builder.RegisterType<Fakultet>().As<IFakultet>();
-            builder.RegisterType<FakultetEntity>().As<IFakultetEntity>();
-            builder.RegisterType<FakultetInput>().As<IFakultetInput>();
-
-            builder.RegisterType<StudentService>().As<IStudentService>();
-            builder.RegisterType<FakultetService>().As<IFakultetService>();
-
-            builder.RegisterType<StudentRepository>().As<IStudentRepository>();
-            builder.RegisterType<FakultetRepository>().As<IFakultetRepository>();
+            builder.RegisterModule<Service.DIModule>();
+            builder.RegisterModule<Repository.DIModule>();
 
             return builder.Build();
         }
